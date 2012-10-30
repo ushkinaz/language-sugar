4d1f66b40d5de57dcaf8dbf65cbd1d08ff58e467
Dmitry Sidorenko <dsidorenko@agent.ru>
2012-10-30 15:51:28 +0400

    rebuild

--- languages/sugar/source_gen/baseLanguage/sugar/dataFlow/StringFormat_DataFlow.java
+++ languages/sugar/source_gen/baseLanguage/sugar/dataFlow/StringFormat_DataFlow.java
@@ -15,11 +15,13 @@ public class StringFormat_DataFlow extends DataFlowBuilder {
   }
 
   public void build(final IOperationContext operationContext, final DataFlowBuilderContext _context) {
-    Iterator<SNode> param_it = ListSequence.fromList(SLinkOperations.getTargets(_context.getNode(), "parameters", true)).iterator();
-    SNode param_var;
-    while (param_it.hasNext()) {
-      param_var = param_it.next();
-      _context.getBuilder().build((SNode) param_var);
+    {
+      Iterator<SNode> param_it = ListSequence.fromList(SLinkOperations.getTargets(_context.getNode(), "parameters", true)).iterator();
+      SNode param_var;
+      while (param_it.hasNext()) {
+        param_var = param_it.next();
+        _context.getBuilder().build((SNode) param_var);
+      }
     }
     _context.getBuilder().build((SNode) SLinkOperations.getTarget(_context.getNode(), "stringFormat", true));
   }
